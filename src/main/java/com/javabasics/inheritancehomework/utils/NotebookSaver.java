6def1680cce4c08c313c02b0fa5dc5cae42cc27c
uladzislau_andrzheyeuski <hans.huhelharter@gmail.com>
2021-10-20 03:55:50 +0300

    refactor NotebookSaver class

--- src/main/java/com/javabasics/inheritancehomework/utils/NotebookSaver.java
+++ src/main/java/com/javabasics/inheritancehomework/utils/NotebookSaver.java
@@ -1,16 +1,17 @@
 package com.javabasics.inheritancehomework.utils;
 
+import com.javabasics.inheritancehomework.notepad.NoteBookProvider;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class NotebookSaver {
-    private final static String OUTPUT_FILE_NAME = "notebook.xml";
-
-    public static void saveNotebookToFile(String xmlString)  {
+    public static void saveNotebookToFile(String outputFileName)  {
         try{
-            FileOutputStream fileOutputStream = new FileOutputStream(OUTPUT_FILE_NAME);
-            fileOutputStream.write(xmlString.getBytes(StandardCharsets.UTF_8));
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFileName);
+            fileOutputStream.write(new XMLGenerator(NoteBookProvider.getNoteBookProvider().getNoteBook())
+                    .generateXml().getBytes(StandardCharsets.UTF_8));
             fileOutputStream.close();
         } catch (IOException e){
             e.printStackTrace();
